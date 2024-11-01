8ed18416f8ac7682481effbd58c32bede0923552
lifang <over.li@flip.fun>
2024-11-01 15:08:53 +0800

    html2md

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello world!!!")
+
+    let url = "https://rust-lang.com/";
+
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    println!("body = {body:?}");
+
+    let md = html2md::parse_html(&body);
+
+    let result = fs::write("rust.md", md.as_bytes());
+    
+    match result { 
+        Ok(_) => println!("File written successfully"),
+        Err(e) => println!("Error writing file: {}", e),
+    }
 }

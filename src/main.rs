1b483b1bf7a5640f6545fb2c18b1d430e09cf48b
lifang <over.li@flip.fun>
2024-11-15 13:41:23 +0800

    demo-str

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 fn main(){
-    let v = factorial(10);
-    println!("v = {}", v)
-}
 
-fn factorial(n:u32) -> u32{
-    if n == 0 || n == 1 {
-        1
-    }else {
-        n * factorial(n - 1)
-    }
+    let v = "hello world".to_string();
+
+    let s1 = &v[..5];
+    let s2 = &v[6..];
+
+    println!("{:?}", s1);
+    println!("{:?}", s2);
+
+
+    let list = vec![1, 2, 3, 4, 5, 6];
+
+    let l1 = &list[2..3];
+    println!("{:?}", l1);
 }

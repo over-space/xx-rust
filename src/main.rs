a26096e9c78f74461bd74e4c650db497323dfd6d
lifang <over.li@flip.fun>
2024-11-16 10:03:29 +0800

    demo-crate

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
+use xx_rust::mod_c;
+
+
 fn main(){
 
-    let v = "hello world".to_string();
+    mod_c::test();
 
-    let s1 = &v[..5];
-    let s2 = &v[6..];
-
-    println!("{:?}", s1);
-    println!("{:?}", s2);
-
-
-    let list = vec![1, 2, 3, 4, 5, 6];
-
-    let l1 = &list[2..3];
-    println!("{:?}", l1);
 }

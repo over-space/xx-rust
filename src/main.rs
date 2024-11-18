fb8481503327d756fd6097d72eca3995ae63f0e7
lifang <over.li@flip.fun>
2024-11-18 13:50:40 +0800

    demo-traitV2

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,42 @@
-use core::prelude::v1;
-use std::fmt::Debug;
+use std::{fmt::Debug, io::Write};
 
-
-
-struct BufBuilder{
+// #[derive(Debug)]
+struct BufBuilder {
+    size: u32,
     buf: Vec<u8>,
-    size: i32,
 }
 
 impl BufBuilder {
-    fn new(size: i32) -> Self {
-        BufBuilder { 
-            buf: Vec::with_capacity(1024), 
-            size,
+    fn new() -> Self {
+        BufBuilder {
+            size: 1024,
+            buf: Vec::with_capacity(1024),
         }
-    }
-    fn add(x: i32){
-        println!("x = {}", x)
-    }
-    fn fun_add(v1: Self, v2: Self) -> i32{
-        v1.size + v2.size
-    }
-
-    fn fun_add2(&self, v2: Self) -> i32{
-        self.size + v2.size
     }
 }
 
 impl Debug for BufBuilder {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", String::from_utf8_lossy(&self.buf))
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BufBuilder")
+            .field("buf", &self.buf)
+            .field("size", &self.size)
+            .finish()
     }
 }
 
-#[derive(Debug, Clone)]
-struct Book{
-    pages: i32,
-    author: String,
-    title: String,
-}
+impl Write for BufBuilder {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.buf.extend_from_slice(buf);
+        Ok(buf.len())
+    }
 
-impl Book {
-    fn print(&self){
-        println!("Book Title: {}, Author: {}, Pages: {}", self.title, self.author, self.pages);
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
     }
 }
-fn print_book(book:Book){
-    println!("Book Title: {}, Author: {}, Pages: {}", book.title, book.author, book.pages);
-}
 
-fn main(){
-    let buf = BufBuilder::new(1);
-    BufBuilder::add(33);
-    println!("{:?}", buf);
-
-    let v1 = BufBuilder::new(2);
-
-    let v2 = BufBuilder::new(5);
-
-    let v3 = BufBuilder::fun_add(v1, v2);
-    println!("{:?}", v3);
-
-    let v4 = BufBuilder::new(2);
-    let v5 = BufBuilder::new(5);
-    let v6 = v4.fun_add2(v5);
-    println!("{:?}", v6);
-
-    let book = Book{
-            pages: 100,
-            author: "John Doe".to_string(),
-            title: "Book Title".to_string(),
-        };
-
-    book.print();
-
-    print_book(book.clone());
-
-    println!("{:?}", book)
-
+fn main() {
+    let mut buf = BufBuilder::new();
+    buf.write_all(b"hello world!!!").unwrap();
+    println!("{:?}", buf)
 }

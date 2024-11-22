7312add46752d6803d9b62dee15eedc9ad242b51
lifang <over.li@flip.fun>
2024-11-22 14:25:12 +0800

    a

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,28 @@
-use std::{fmt::Debug, io::Write};
+use std::{fs::File, io::Read, path::Display};
 
-// #[derive(Debug)]
-struct BufBuilder {
-    size: u32,
-    buf: Vec<u8>,
+struct Book{
+    name: String,
+    author: String,
 }
 
-impl BufBuilder {
-    fn new() -> Self {
-        BufBuilder {
-            size: 1024,
-            buf: Vec::with_capacity(1024),
-        }
-    }
-}
-
-impl Debug for BufBuilder {
+impl Display for Book{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("BufBuilder")
-            .field("buf", &self.buf)
-            .field("size", &self.size)
-            .finish()
-    }
-}
-
-impl Write for BufBuilder {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.buf.extend_from_slice(buf);
-        Ok(buf.len())
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        Ok(())
+        write!(f, "{} by {}", self.name, self.author)
     }
 }
 
 fn main() {
-    let mut buf = BufBuilder::new();
-    buf.write_all(b"hello world!!!").unwrap();
-    println!("{:?}", buf)
+
+    let mut file = File::open("jdbc.txt").unwrap();
+    let mut contents = String::new();
+
+
+    // 将文件内容读取到字符串中
+    file.read_to_string(&mut contents).expect("无法读取文件内容");
+
+    for line in contents.lines() {
+        let parts: Vec<&str> = line.split('/').map(|x| x.trim()).collect();
+        println!("{},{}", parts[0], parts[parts.len() - 1]);
+    }
+
 }

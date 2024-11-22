04c4a1bbd677550b88d7ece2f6e15e58d978e051
lifang <over.li@flip.fun>
2024-11-22 18:42:42 +0800

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,108 @@
-use std::{fs::File, io::Read, path::Display};
+use std::{ops::Deref, thread};
+use common::random_number;
 
-struct Book{
-    name: String,
+struct Book<T>{
+    title: String,
     author: String,
-}
-
-impl Display for Book{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} by {}", self.name, self.author)
-    }
+    price: T
 }
 
 fn main() {
 
-    let mut file = File::open("jdbc.txt").unwrap();
-    let mut contents = String::new();
+    let a = |x| {x * 2};
 
+    println!("a = {}", a(2));
 
-    // 将文件内容读取到字符串中
-    file.read_to_string(&mut contents).expect("无法读取文件内容");
+    let x = 3;
 
-    for line in contents.lines() {
-        let parts: Vec<&str> = line.split('/').map(|x| x.trim()).collect();
-        println!("{},{}", parts[0], parts[parts.len() - 1]);
+    let b = |b|{b + x};
+
+    println!("a = {}, x= {}", b(2), x);
+
+    let add = |a, b, c|{a + b + c};
+
+    println!("a = {}", add(1, 2, 3));
+
+    let add = |a, b, c|{a + b + c};
+
+    println!("a = {}", add(1.1, 2.2, 3.3));
+
+    let t1 = thread::spawn(|| {
+            let n = thread::current().name().unwrap_or("Unnamed").to_string();
+            for i in 1..10 {
+                println!("子线程1, name = {}, i = {}", n, i)
+            }
+        });
+
+    thread::Builder::new().name("thread-1".to_string()).spawn(||{
+            let n = thread::current().name().unwrap_or("Unnamed").to_string();
+            for i in 1..10 {
+                println!("子线程2, name = {}, i = {}", n, i)
+            }
+        }).unwrap();
+
+    t1.join().unwrap();
+    // thread::sleep(Duration::from_secs(10));
+
+    let a1 = is_even(4);
+    println!("a = {:?}", a1);
+
+    let a1 = is_even(5);
+    println!("a = {:?}", a1);
+
+    let a = 32;
+    let b = Box::new(a);
+    let c = *b;
+    println!("a: {}, b: {}", a, b);
+    println!("{}", 32 == a);
+    println!("{}", 32 == *b);
+
+    let a = "hello rust!!!".to_string();
+    let b = Box::new(a);
+    println!("a: {}, b: {}", "none".to_owned(), b);
+    
+    let a = CustomBox{
+            value: 100,
+        };
+
+    println!("a: {:?}", a);
+    println!("100 == *a : {}", 100 == *a);    
+
+    // let secret_number = rand::thread_rng().gen_range(10..20);
+    // println!("secret_number = {}", secret_number);
+    let a = random_number::gen_random_number(10, 1000);
+    println!("secret_number = {}", a);
+
+    println!("main方法执行完毕！");
+
+}
+
+#[derive(Debug)]
+struct CustomBox<T>{
+    value: T,
+}
+
+impl<T> Deref for CustomBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.value
     }
+}
 
+
+impl<T> Drop for CustomBox<T> {
+    fn drop(&mut self) {
+        println!("CustomBox drop");
+    }
+}
+
+
+fn is_even(n : i32) -> Result<bool, String>{
+    if n % 2 == 0{
+        Ok(true)
+    }else{
+        // panic!("程序崩溃了");
+        Err(format!("{} is not even", n))
+    }
 }

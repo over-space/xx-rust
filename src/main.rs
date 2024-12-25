4d64d6adf5ae48a3dea514f23b809251726ffc75
lifang <over.li@flip.fun>
2024-12-25 20:16:11 +0800

    painc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello world!!!")
+    println!("Hello world!!!");
+
+    let a = gift(None);
+    let b = gift(Some("cat"));
+    let c = gift(Some("snake"));
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", c);
+
+    a.map(|f| println(f));
+
+    let d = b.map_or("default", |f| "google.com");
+    println(d);
+    let d = b.map_or("default", |f| "google.com");
+    println(d);
+
+    let fun = |a| println(a);
+    a.map(fun);
+    b.map(fun);
+    c.map(fun);
+}
+
+fn println(v:&str){
+    println!("v = {}", v);
+}
+
+fn gift(gift: Option<&str>) -> Option<&str>{
+    match gift {
+        Some("snake") => println!("Yuck! I'm throwing that snake in a fire."),
+        Some(v) => println!("{}? How nice.", v),
+        None => println!("No gift selected."),
+    }
+    // gift.expect("not gift");
+    let a = gift?;
+    Some(a)
 }
